c1d9c327ad6176f4ed3a8da8dcf58ed66f416bfe
Mei Giyanto <denmasgie0@gmail.com>
2026-08-11 13:46:53 +0000

    modifikasi berkas login-form.tsx menjadi login-form.txt

--- src/components/site-shell.tsx
+++ src/components/site-shell.tsx
@@ -105,7 +105,7 @@ export default function SiteShell({ children }: { children: React.ReactNode }) {
             )} */}
 
             <Show when="signed-out">
-              <SignInButton className="hover:text-slate-900"/>
+              <SignInButton/>
               <SignUpButton>
                 {/* <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer"> */}
                   Sign Up
@@ -114,7 +114,7 @@ export default function SiteShell({ children }: { children: React.ReactNode }) {
             </Show>
             
             <Show when="signed-in">
-              <UserButton className="hover:text-slate-900"/>
+              <UserButton/>
             </Show>
 
           </div>
