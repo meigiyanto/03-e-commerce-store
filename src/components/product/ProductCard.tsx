5844f6d555d6aee714c112f5c231ea278e7206a0
meigiyanto <denmasgie0@gmail.com>
2026-09-13 23:13:15 +0700

    membuat halaman shopping cart

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -12,7 +12,7 @@ export default function ProductCard({ product, }: ProductCardProps) {
     const handleAddToCart = () => { addItem(product); };
     
     return (
-        <Link href={/products/${product.id}}>    
+        <Link href={`/products/${product.id}`}>    
             
             {/* Product Content */}
             <div className="p-4">
