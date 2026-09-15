0a91a5299c752a1e68ebf111ae57fa72ab06971f
meigiyanto <denmasgie0@gmail.com>
2026-09-15 17:52:05 +0700

    refaktori lanjutan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { Metadata } from "next";
 import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
