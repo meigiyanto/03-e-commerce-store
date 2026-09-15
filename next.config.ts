f37f98b24c0dc8a49ab503594b7a065df5c64b97
meigiyanto <denmasgie0@gmail.com>
2026-09-15 10:35:30 +0700

    fix image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [new URL('https://assets.example.com/account123/**')],
+  },
 };
 
 export default nextConfig;
