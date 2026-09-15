03fb50bccf4ffc36c113f6fe89207b4f392e9b50
meigiyanto <denmasgie0@gmail.com>
2026-09-15 15:39:18 +0700

    refaktori lanjutan

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
     const search = searchParams.get("search");
     const products = await prisma.product.findMany({
       where: {
-        ...(category ? { category,}} : {}),
+        ...(category ? { category, } : {}),
         ...(search
           ? {
               OR: [
