cb78134717cbd4b3b27c9aa11e55e7196bbc9631
meigiyanto <denmasgie0@gmail.com>
2026-09-17 09:40:59 +0700

    refaktori lanjutan

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+export function Label({
+  className = "",
+  ...props
+}: LabelProps) {
+  return (
+    <label
+      className={`text-sm font-medium leading-none ${className}`}
+      {...props}
+    />
+  );
+}
